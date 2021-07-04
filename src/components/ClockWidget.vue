7f8f4459f60e3d72f6b801e40f8e6ffd7db7a7b5
vladze0x04 <vladze.0x04@gmail.com>
2021-07-04 12:31:30 +0800

    Clock sample component

---
+++ src/components/ClockWidget.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="clock-widget">
+    <h4 class="clock-title">This is the current time</h4>
+    <p class="clock-time">{{ currentDateTimeString }}</p>
+  </div>
+</template>
+
+<script>
+const momentTz = require("moment-timezone");
+const SECOND_IN_MILLISECONDS = 1000;
+export default {
+  name: "ClockWidget",
+  data() {
+    return {
+      currentDateTime: momentTz().tz("Asia/Manila"),
+      dateTimeUpdater: null,
+    };
+  },
+  created() {
+    this.dateTimeUpdater = setInterval(
+      this.updateDateTime,
+      SECOND_IN_MILLISECONDS
+    );
+  },
+  computed: {
+    currentDateTimeString() {
+      return this.currentDateTime.format();
+    },
+  },
+  methods: {
+    updateDateTime() {
+      this.currentDateTime = momentTz().tz("Asia/Manila");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.clock-widget * {
+  margin: 0;
+}
+</style>
