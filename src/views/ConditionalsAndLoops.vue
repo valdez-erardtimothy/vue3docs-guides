f8e7751a148fbf85586059da557d1ccd767d7c91
vladze0x04 <vladze.0x04@gmail.com>
2021-07-05 15:34:38 +0800

    conditionals and loops

--- src/views/ConditionalsAndLoops.vue
+++ src/views/ConditionalsAndLoops.vue
@@ -7,12 +7,24 @@
       </button>
       <p class="p-2" v-if="seen">Now you see me, then what?</p>
     </div>
+    <div id="list-rendering" class="text-left">
+      <h1 class="text-center">List Rendering</h1>
+      <ul>
+        <li class="px-2 py-3" :key="todo.id" v-for="todo in todos">
+          {{ todo.text }}
+        </li>
+      </ul>
+    </div>
+    <div id="through-bind"></div>
   </div>
 </template>
 
 <style lang="postcss" scoped>
 div {
-  @apply py-4 bg-gray-200;
+  @apply py-4;
+}
+div > div {
+  @apply py-4 my-1 border-b-4 border-gray-700 border-opacity-25;
 }
 </style>
 
@@ -22,14 +34,17 @@ export default {
   data() {
     return {
       seen: false,
+      todos: [
+        { id: 1, text: "Learn JavaScript" },
+        { id: 2, text: "Learn Vue" },
+        { id: 3, text: "Build something awesome" },
+      ],
     };
   },
-  methods() {
-    return {
-      toggleSeen() {
-        this.seen != this.seen;
-      },
-    };
+  methods: {
+    toggleSeen() {
+      this.seen = !this.seen;
+    },
   },
 };
 </script>
