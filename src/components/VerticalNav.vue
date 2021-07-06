55a2413b5436d03b368a1d9253bf6109ea3c9d2f
vladze0x04 <vladze.0x04@gmail.com>
2021-07-07 05:47:00 +0800

    vertical nav changes

--- src/components/VerticalNav.vue
+++ src/components/VerticalNav.vue
@@ -1,20 +1,39 @@
+<!-- WIP -->
 <template>
   <nav class="sidenav">
-    <p class="sidenav-item"><router-link to="/">Home</router-link></p>
-    <p class="sidenav-item"><router-link to="/about">About</router-link></p>
-    <p class="sidenav-item">
-      <router-link to="/conditionals-and-loops">
-        Conditionals and Loops
-      </router-link>
-    </p>
+    <div class="sidenav-item">
+      <p>Introduction</p>
+      <div class="sidenav-subitem">
+        <router-link to="/">Home</router-link>
+      </div>
+      <div class="sidenav-subitem">
+        <router-link to="/about">About</router-link>
+      </div>
+      <div class="sidenav-subitem">
+        <router-link to="/conditionals-and-loops">
+          Conditionals and Loops
+        </router-link>
+      </div>
+    </div>
   </nav>
 </template>
 
 <style lang="postcss" scoped>
-.sidenav > .sidenav-item {
-  @apply py-4 px-5 font-light;
+.sidenav {
+  text-align: left;
 }
-.sidenav > .sidenav-item:hover {
+.sidenav .sidenav-item,
+.sidenav .sidenav-subitem {
+  @apply pt-4 px-5 font-light;
+}
+
+/* reconsider design choice later. */
+.sidenav .sidenav-link,
+.sidenav .sidenav-subitem:hover {
   @apply bg-gray-300 font-medium;
+}
+
+.sidenav .sidenav-item .sidenav-subitem {
+  @apply pl-4;
 }
 </style>
